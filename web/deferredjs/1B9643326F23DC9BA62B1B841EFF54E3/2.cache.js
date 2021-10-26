0bbcb963c70aa1333f5f8e78b4aee0d6217df230
syjsu <syjsu@qq.com>
2021-10-26 14:55:00 +0800

    init

---
+++ web/deferredjs/1B9643326F23DC9BA62B1B841EFF54E3/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.web.runAsyncCallback2('mEc(3401,1,Esm);_.we=function(){};IEl(Oi)(2);\n//# sourceURL=web-2.js\n')
---
+++ webSimple/deferredjs/63C637244C02B92F15F8315EC881711B/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.webSimple.runAsyncCallback2('EDb(1989,1,lOh);_.Gd=function(){};Qch(dh)(2);\n//# sourceURL=webSimple-2.js\n')
